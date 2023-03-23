0755c09baf447e6cb4d39c7a026889308d4e0d99
sp020499 <128695221+sp020499@users.noreply.github.com>
2023-03-24 00:21:51 +0530

    Update fhir-client-v0.1.12.js

--- example-smart-app/lib/js/fhir-client-v0.1.12.js
+++ example-smart-app/lib/js/fhir-client-v0.1.12.js
@@ -17418,10 +17418,7 @@ BBClient.authorize = function(params, errback){
       "scope="+encodeURIComponent(client.scope)+"&"+
       "redirect_uri="+encodeURIComponent(client.redirect_uri)+"&"+
       "state="+encodeURIComponent(state)+"&"+
-      "aud="+encodeURIComponent(params.server)+"&"+
-	"cerner_StudyIdentifier="+encodeURIComponent('UID123')+"&"+
-	"cerner_View_Mode="+encodeURIComponent('False')+"&"+
-	"cerner_Accession="+encodeURIComponent('000CV2023001234')";
+      "aud="+encodeURIComponent(params.server);
     
     if (typeof client.launch !== 'undefined' && client.launch) {
        redirect_to += "&launch="+encodeURIComponent(client.launch);
