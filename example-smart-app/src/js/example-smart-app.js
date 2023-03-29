31c120d19523cbe580a3c02c06e9c9c7b73f8001
sp020499 <128695221+sp020499@users.noreply.github.com>
2023-03-30 00:52:09 +0530

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -33,6 +33,7 @@
 
           if (typeof patient.name[0] !== 'undefined') {
             fname = patient.name[0].given.join(' ');
+            lname = patient.name[0].family.join(' ');
             if(Array.isArray(patient.name[0].family)){
               lname = patient.name[0].family.join(' ');
             }
